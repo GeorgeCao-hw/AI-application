826c62108fae8b7a5e41214b8af63ab9264a5f5b
GeorgeCao-hw <caozhi1214@gmail.com>
2026-01-26 19:34:02 +0800

    update main.py

--- mini_claude_code/main.py
+++ mini_claude_code/main.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     agent = Agent()
 
     agent.run(
-        "Create a Python file demo.py that prints 'hello from agent'. "
+        "Create a Python file demo.py that prints 'hello from agent'. need to run independently. "
+        "Include main fun"
         "If the file exists, update it."
     )
 
